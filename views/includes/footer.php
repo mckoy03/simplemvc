52bcec22226acb39470ac2fdb56f23106fb9097e
Mark Cawit <markcawit@gmail.com>
2016-09-15 21:37:52 +0800

    adding assets folder structure

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,3 +1,5 @@
 		<div id="footer">Footer</div>
+
+		<script type="text/javascript" src="/assets/js/app.js"></script>
 	</body>
 </html>
--- views/includes/header.php
+++ views/includes/header.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<title>Home &mdash; <?php echo SITE_NAME ?></title>	
+		<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 	</head>
 
 	<body>
